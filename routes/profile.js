836c2f23df95b7df36b7a6983bc68bba54ca5d00
thegouge <thegouge42@gmail.com>
2019-08-14 11:40:02 -0700

    set up backend with Ovrstat

---
+++ routes/profile.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const fetch = require("node-fetch");
+
+router.get("/:platform/:gamertag", async (req, res) => {
+  try {
+    const headers = {
+      Accept: "application/json",
+      "Accept-Encoding": "gzip",
+    };
+
+    const {platform, gamertag} = req.params;
+
+    const response = await fetch(
+      `${process.env.TRACKER_API_URL}/${platform}/${gamertag}`,
+      {headers}
+    );
+
+    const data = await response.json();
+
+    // if (data.errors && data.errors.length > 0) {
+    //   return res.status(404).json({
+    //     message: "profile not found",
+    //   });
+    // }
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "something went wrong on the server",
+    });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({path: "./config.env"});
+
+const app = express();
+
+// Profile Routes
+app.use("/api/test", (req, res) => {
+  res.send("hello!");
+});
+app.use("/api/v1/profile", require("./routes/profile"));
+
+// Handle Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public/"));
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`${process.env.NODE_ENV} server running on port ${port}`);
+});
