a9f33f96a1be11322d41705cae4699663228ed11
thegouge <thegouge42@gmail.com>
2020-09-24 19:53:54 -0700

    formatting changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-import "./fonts.css";
+import './fonts.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
