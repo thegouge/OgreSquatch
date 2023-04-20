150edc9c43cc3a541e56c16dfeb5c717de42ac10
Alex Gouge-Schajer <alexg@accuglassproducts.com>
2023-04-20 16:53:37 -0700

    following React Router's docs

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+import Search from '../components/Search'
+import Profile from '../components/Profile'
 
-import Search from '../components/Search';
-import Profile from '../components/Profile';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" exact component={Search}>
+      <Route path="/profile/:platform/:gamertag" component={Profile} />
+    </Route>
+  )
+)
 
-export default () => {
-	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/" exact component={Search} />
-				<Route path="/profile/:platform/:gamertag" component={Profile} />
-			</Switch>
-		</BrowserRouter>
-	);
-};
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
