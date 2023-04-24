195a22846a91070ade542e7f56a7159f34f32e6f
Alex Gouge-Schajer <alexg@accuglassproducts.com>
2023-04-24 09:21:47 -0700

    re-vamping the trimmedData stuff to be more valid

--- server/lib/trimmedData.js
+++ server/lib/trimmedData.js
@@ -1,110 +1,33 @@
-const heroList = require("./masterHeroList.js");
-
-class formattedData {
-  constructor(data = {name: "default", private: true}, platform) {
-    if (data.private) {
-      this.name = data.name;
-      this.private = true;
-      this.error = "private";
-    } else {
-      let name = data.name;
-      if (platform === "pc") {
-        name = data.name.replace(/#\d+$/, "");
-      }
-      this.profile = {
-        name,
-        level: data.level,
-        endorsement: data.endorsement,
-        prestige: data.prestige,
-        rating: data.rating,
-        gamesWon: data.gamesWon,
-        icons: {
-          profileIcon: data.icon,
-          levelIcon: data.levelIcon,
-          endorsementIcon: data.endorsementIcon,
-          prestigeIcon: data.prestigeIcon,
-          ratingIcon: data.ratingIcon,
-        },
-      };
-
-      this.heroes = mapThroughHeroes(
-        data.quickPlayStats,
-        data.competitiveStats
-      );
-    }
-  }
-}
-
-function mapThroughHeroes(quickData, compData) {
-  const result = {};
-  heroList.forEach((hero) => {
-    const trimmedQuick = trimHeroData(
-      quickData.careerStats[hero],
-      quickData.topHeroes[hero]
-    );
-    const trimmedComp = trimHeroData(
-      compData.careerStats[hero],
-      compData.topHeroes[hero]
-    );
-
-    switch (hero) {
-      case "allHeroes":
-        hero = "all-Heroes";
-        break;
-
-      case "dVa":
-        hero = "dva";
-        break;
-
-      case "soldier76":
-        hero = "soldier-76";
-        break;
-
-      case "wreckingBall":
-        hero = "wrecking-ball";
-        break;
-
-      default:
-        break;
-    }
-
-    result[hero] = {
-      name: hero,
-      quick: trimmedQuick,
-      comp: trimmedComp,
-    };
-  });
-  return result;
-}
+const heroList = require('./masterHeroList.js')
 
 function trimHeroData(heroData, top) {
   if (!heroData) {
-    return "no data!";
+    return 'no data!'
   }
   const defaultValues = {
-    assists: {defensiveAssists: 0, offensiveAssists: 0, reconAssists: 0},
-    combat: {damageDone: 0, deaths: 0},
-    game: {gamesWon: 0, timePlayed: "0:0:0"},
-    matchAwards: {medals: 0, medalsBronze: 0, medalsSilver: 0, medalsGold: 0},
-  };
+    assists: { defensiveAssists: 0, offensiveAssists: 0, reconAssists: 0 },
+    combat: { damageDone: 0, deaths: 0 },
+    game: { gamesWon: 0, timePlayed: '0:0:0' },
+    matchAwards: { medals: 0, medalsBronze: 0, medalsSilver: 0, medalsGold: 0 },
+  }
 
   Object.keys(defaultValues).forEach((cat) => {
     if (!heroData[cat]) {
-      heroData[cat] = defaultValues[cat];
+      heroData[cat] = defaultValues[cat]
     }
     Object.keys(defaultValues[cat]).forEach((subCat) => {
       if (!heroData[cat][subCat]) {
-        heroData[cat][subCat] = defaultValues[cat][subCat];
+        heroData[cat][subCat] = defaultValues[cat][subCat]
       }
-    });
-  });
+    })
+  })
 
-  const {assists, combat, game, matchAwards} = heroData;
+  const { assists, combat, game, matchAwards } = heroData
 
   const addedAssists =
-    assists.defensiveAssists + assists.offensiveAssists + assists.reconAssists;
+    assists.defensiveAssists + assists.offensiveAssists + assists.reconAssists
 
-  const topHero = top ? true : false;
+  const topHero = top !== undefined
 
   return {
     assists: addedAssists,
@@ -120,7 +43,101 @@ function trimHeroData(heroData, top) {
       silver: matchAwards.medalsSilver,
       gold: matchAwards.medalsGold,
     },
-  };
+  }
 }
 
-module.exports = formattedData;
+function mapThroughHeroes(quickData, compData) {
+  const result = {}
+  heroList.forEach((hero) => {
+    const trimmedQuick = trimHeroData(
+      quickData.careerStats[hero],
+      quickData.topHeroes[hero]
+    )
+    const trimmedComp = trimHeroData(
+      compData.careerStats[hero],
+      compData.topHeroes[hero]
+    )
+
+    let heroName = hero
+
+    switch (hero) {
+      case 'allHeroes':
+        heroName = 'all-Heroes'
+        break
+
+      case 'dVa':
+        heroName = 'dva'
+        break
+
+      case 'soldier76':
+        heroName = 'soldier-76'
+        break
+
+      case 'wreckingBall':
+        heroName = 'wrecking-ball'
+        break
+
+      default:
+        break
+    }
+
+    result[heroName] = {
+      name: heroName,
+      quick: trimmedQuick,
+      comp: trimmedComp,
+    }
+  })
+  return result
+}
+
+class formattedData {
+  constructor(
+    {
+      name,
+      privateAccount,
+      level,
+      endorsement,
+      prestige,
+      rating,
+      gamesWon,
+      icon,
+      levelIcon,
+      endorsementIcon,
+      prestigeIcon,
+      ratingIcon,
+      quickPlayStats,
+      competitiveStats,
+    },
+    platform
+  ) {
+    if (privateAccount) {
+      this.name = name
+      this.privateAccount = true
+      this.error = 'private'
+    } else {
+      let userName = name
+      if (platform === 'pc') {
+        userName = name.replace(/#\d+$/, '')
+      }
+      this.profile = {
+        name: userName,
+        level,
+        endorsement,
+        prestige,
+        rating,
+        gamesWon,
+        icons: {
+          profileIcon: icon,
+          levelIcon,
+          endorsementIcon,
+          prestigeIcon,
+          ratingIcon,
+        },
+      }
+
+      this.heroes = mapThroughHeroes(quickPlayStats, competitiveStats)
+    }
+  }
+}
+
+module.exports = formattedData
